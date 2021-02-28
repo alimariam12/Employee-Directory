a3eda56e38d3841695a6f0b0491a73a271a2afaa
Mariam Ali <alim12@csp.edu>
2021-02-28 15:30:56 -0600

    working on data

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-// Export an object containing methods we'll use for accessing the Dog.Ceo API
-
 export default {
   getRandomUser: function() {
-    return axios.get("https://randomuser.me/api/?results=100");
+    return axios.get("https://randomuser.me/api/?results=50");
   },
+
 };
